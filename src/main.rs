8409acbb681228642b25241ae6b942b534075e94
Matthias Reitinger <reima@edenprime.de>
2014-02-03 11:41:31 +0100

    Replaced fixed file name by command line option

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ mod disasm;
 mod mem;
 
 fn main() {
-  let mut d = disasm::Disasm;
+  let args = std::os::args();
+  if args.len() != 2 {
+    println!("Usage: {:s} rom.gb", args[0]);
+    return;
+  }
 
-  let mut cart = cartridge::Cartridge::from_path(&Path::new("tetris.gb"));
+  let mut cart = cartridge::Cartridge::from_path(&Path::new(args[1]));
+  let mut d = disasm::Disasm;
   let mut pc = 0u16;
   while pc < 0x8000u16 - 0x150u16 {
     let temp = pc;
     println!("{:04X}: {:s}", temp + 0x150, cpu::decode(&mut cart.rom, &mut pc, &mut d));
   }
 }
+
