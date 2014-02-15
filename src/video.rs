8bda9ac57c79b9fda477d1b358212f1c2ecc440b
Matthias Reitinger <reima@edenprime.de>
2014-02-15 01:25:42 +0100

    Added basic obj (sprite) drawing

--- src/video.rs
+++ src/video.rs
@@ -15,19 +15,46 @@ static MODE3_START: u32 = MODE2_START + MODE2_CYCLES;
 
 static ROW_CYCLES: u32 = MODE0_CYCLES + MODE2_CYCLES + MODE3_CYCLES;
 
-static ROW_COUNT: u8 = 144; // After this many rows, V-Blank starts
+static SCREEN_REFRESH_CYCLES: u32 = SCREEN_HEIGHT as u32 * ROW_CYCLES + MODE1_CYCLES;
 
-static SCREEN_REFRESH_CYCLES: u32 = ROW_COUNT as u32 * ROW_CYCLES + MODE1_CYCLES;
+static STAT_MODE0_IRQ: u8        = 0b0000_1000;
+static STAT_MODE1_IRQ: u8        = 0b0001_0000;
+static STAT_MODE2_IRQ: u8        = 0b0010_0000;
+static STAT_COINCIDENCE_IRQ: u8  = 0b0100_0000;
+static STAT_IRQ_MASK: u8         = 0b0111_1000;
+static STAT_COINCIDENCE_FLAG: u8 = 0b0000_0100;
 
-static STAT_MODE0_IRQ: u8        = 0b00001000;
-static STAT_MODE1_IRQ: u8        = 0b00010000;
-static STAT_MODE2_IRQ: u8        = 0b00100000;
-static STAT_COINCIDENCE_IRQ: u8  = 0b01000000;
-static STAT_IRQ_MASK: u8         = 0b01111000;
-static STAT_COINCIDENCE_FLAG: u8 = 0b00000100;
+static FLAG_ENABLE_BG_WIN: u8 = 0b0000_0001;
+static FLAG_ENABLE_OBJ: u8    = 0b0000_0010;
+static FLAG_OBJ_SIZE: u8      = 0b0000_0100;
+static FLAG_BG_MAP: u8        = 0b0000_1000;
+static FLAG_BG_WIN_TILES: u8  = 0b0001_0000;
+static FLAG_ENABLE_WIN: u8    = 0b0010_0000;
+static FLAG_WIN_MAP: u8       = 0b0100_0000;
+static FLAG_ENABLE: u8        = 0b1000_0000;
+
+static TILES_BASE0: uint = 0x800;
+static TILES_BIAS0: u8 = 128u8;
+static TILES_BASE1: uint = 0x000;
+static TILES_BIAS1: u8 = 0u8;
+
+static BG_WIN_MAP_BASE0: uint = 0x1800;
+static BG_WIN_MAP_BASE1: uint = 0x1c00;
+
+static TILE_WIDTH: uint  = 8;
+static TILE_HEIGHT: uint = 8;
+static TILE_BYTES: uint  = 16;
+
+static BG_WIDTH_TILES:  uint = 32;
+static BG_HEIGHT_TILES: uint = 32;
+
+static OBJ_FLAG_PALETTE:  u8 = 0b0001_0000;
+static OBJ_FLAG_FLIP_X:   u8 = 0b0010_0000;
+static OBJ_FLAG_FLIP_Y:   u8 = 0b0100_0000;
+static OBJ_FLAG_PRIORITY: u8 = 0b1000_0000;
 
 pub static SCREEN_WIDTH: uint = 160;
-pub static SCREEN_HEIGHT: uint = 144;
+pub static SCREEN_HEIGHT: uint = 144;  // After this many rows, V-Blank starts
 
 pub struct Video {
   // Implementation state
@@ -99,7 +126,7 @@ impl Video {
     self.ly = (self.cycles / ROW_CYCLES) as u8;
 
     self.mode =
-      if self.ly < ROW_COUNT {
+      if self.ly < SCREEN_HEIGHT as u8 {
         match self.cycles % ROW_CYCLES {
           MODE0_START .. (MODE2_START - 1) => 0,
           MODE2_START .. (MODE3_START - 1) => 2,
@@ -126,7 +153,7 @@ impl Video {
 
       if self.mode == 0 {
         // H-Blank
-        self.draw_bg_row(self.ly as uint);
+        self.draw_row(self.ly as uint);
       }
 
       match self.mode {
@@ -154,36 +181,100 @@ impl Video {
       self.stat &= !STAT_COINCIDENCE_FLAG;
     }
     // Mode flag
-    self.stat = (self.stat & (STAT_IRQ_MASK |STAT_COINCIDENCE_FLAG)) | self.mode;
+    self.stat = (self.stat & (STAT_IRQ_MASK | STAT_COINCIDENCE_FLAG)) | self.mode;
+  }
+
+  fn draw_row(&mut self, row: uint) {
+    if (self.flags & FLAG_ENABLE_BG_WIN) != 0 {
+      self.draw_bg_row(row);
+    }
+
+    if (self.flags & FLAG_ENABLE_OBJ) != 0 {
+      self.draw_obj_row(row);
+    }
   }
 
   fn draw_bg_row(&mut self, row: uint) {
-    let mut tile_base = 0x800;
-    let mut tile_bias = 128u8;
-    let mut map_base = 0x1800;
+    let mut tiles_base = TILES_BASE0;
+    let mut tiles_bias = TILES_BIAS0;
+    let mut map_base = BG_WIN_MAP_BASE0;
 
-    if (self.flags & 0b10000) != 0 {
-      tile_base = 0x0;
-      tile_bias = 0u8;
+    if (self.flags & FLAG_BG_WIN_TILES) != 0 {
+      tiles_base = TILES_BASE1;
+      tiles_bias = TILES_BIAS1;
     }
-    if (self.flags & 0b01000) != 0 {
-      map_base = 0x1c00;
+    if (self.flags & FLAG_BG_MAP) != 0 {
+      map_base = BG_WIN_MAP_BASE1;
     }
 
     let pal = self.bgp;
 
     let y = row;
-    let bg_y = (y + self.scy as uint) % (32*8);
-    let bg_tile_y = bg_y / 8;
+    let bg_y = (y + self.scy as uint) % (BG_HEIGHT_TILES * TILE_HEIGHT);
+    let bg_tile_y = bg_y / TILE_HEIGHT;
     for x in range(0u, SCREEN_WIDTH) {
-      let bg_x = (x + self.scx as uint) % (32*8);
-      let bg_tile_x = bg_x / 8;
-      let tile_num = (self.vram[map_base + bg_tile_y*32 + bg_tile_x] + tile_bias) as uint;
-      let tile = self.vram.slice_from(tile_base + tile_num*16);
+      let bg_x = (x + self.scx as uint) % (BG_WIDTH_TILES * TILE_WIDTH);
+      let bg_tile_x = bg_x / TILE_WIDTH;
+      let tile_num = (self.vram[map_base + bg_tile_y*BG_WIDTH_TILES + bg_tile_x] + tiles_bias) as uint;
+      let tile = self.vram.slice_from(tiles_base + tile_num*TILE_BYTES);
       let pixel = self.screen.mut_slice_from((y * SCREEN_WIDTH + x) * 4);
-      let tile_row = bg_y % 8;
-      let tile_col = bg_x % 8;
-      unpack_tile_pixel(tile, pal, tile_row, tile_col, pixel);
+      let tile_row = bg_y % TILE_HEIGHT;
+      let tile_col = bg_x % TILE_WIDTH;
+      unpack_tile_pixel(tile, pal, tile_row, tile_col, pixel, false);
+    }
+  }
+
+  fn draw_obj_row(&mut self, row: uint) {
+    // TODO: 8x16 obj support
+
+    // Obj with coordinates (OFFSET_X, OFFSET_Y) is at (0, 0) on screen
+    static OFFSET_X: uint = 8;
+    static OFFSET_Y: uint = 16;
+
+    static MAX_OBJS_PER_ROW: uint = 10;
+
+    // Find objs in this row
+    let mut objs = ~[];
+    for obj_num in range(0, 40) {
+      let obj_y = self.oam[obj_num * 4] as uint;
+      if obj_y <= row + OFFSET_Y && row + OFFSET_Y < obj_y + TILE_HEIGHT {
+        objs.push(obj_num);
+      }
+    }
+
+    // Early out when no objs in this row
+    if objs.len() == 0 {
+      return;
+    }
+
+    // TODO: Restrict to 10 objs by priority
+
+    // Sort by X coordinate, high to low
+    objs.sort_by(|obj0, obj1|
+      self.oam[(*obj1)*4 + 1].cmp(&self.oam[(*obj0)*4 + 1])
+    );
+
+    // Draw objs
+    for obj in objs.iter() {
+      let obj_y =     self.oam[(*obj) * 4] as uint;
+      let obj_x =     self.oam[(*obj) * 4 + 1] as uint;
+      let obj_tile =  self.oam[(*obj) * 4 + 2] as uint;
+      let obj_flags = self.oam[(*obj) * 4 + 3];
+      let tile = self.vram.slice_from(TILES_BASE1 + obj_tile*TILE_BYTES);
+      let mut tile_row = row - obj_y + OFFSET_Y;
+      if (obj_flags & OBJ_FLAG_FLIP_Y) != 0 {
+        tile_row = TILE_HEIGHT - 1 - tile_row;
+      }
+      for tile_col in range(0, TILE_WIDTH) {
+        let x = obj_x + tile_col;
+        if OFFSET_X <= x && x < SCREEN_WIDTH + OFFSET_X {
+          let pixel = self.screen.mut_slice_from((row * SCREEN_WIDTH + x - OFFSET_X) * 4);
+          if (obj_flags & OBJ_FLAG_FLIP_X) != 0 {
+            tile_col = TILE_WIDTH - 1 - tile_col;
+          }
+          unpack_tile_pixel(tile, self.obp0, tile_row, tile_col, pixel, true);
+        }
+      }
     }
   }
 }
@@ -238,9 +329,10 @@ fn unpack_tile_pixel(tile: &[u8],
                      palette: u8,
                      row: uint,
                      col: uint,
-                     pixel: &mut [u8]) {
+                     pixel: &mut [u8],
+                     transp: bool) {
   static colors: &'static [& 'static[u8]] = &[
-    &[244, 248, 208],
+    &[224, 248, 208],
     &[136, 192, 112],
     &[52, 104, 86],
     &[8, 24, 32]
@@ -250,6 +342,11 @@ fn unpack_tile_pixel(tile: &[u8],
   let high_bit = (tile[2*row+1] >> (7 - col)) & 1;
   let value = (high_bit << 1) | low_bit;
   let color = (palette >> (2 * value)) & 0b11;
+
+  if transp && color == 0 {
+    return;
+  }
+
   pixel[0] = colors[color][2];
   pixel[1] = colors[color][1];
   pixel[2] = colors[color][0];
